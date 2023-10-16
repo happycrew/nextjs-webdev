a7e2a1a71014f5c34a0761512ea744bc877900bb
Vladimir Orlov <satancrew.true@gmail.com>
2023-10-16 15:28:10 +0300

    feat: add styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Heading from '@/components/Heading';
+import styles from '@/styles/Home.module.scss';
 
 const Home = () => {
-  return <Heading text="Hello world" />;
+  return (
+    <div className={styles.wrapper}>
+      <Heading text="Hello world" />
+    </div>
+  );
 };
 
 export default Home;
