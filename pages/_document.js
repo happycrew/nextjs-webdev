8c18a0812c83ca1b14937044da2b0374bd65f9d6
Vladimir Orlov <satancrew.true@gmail.com>
2023-10-16 16:57:10 +0300

    feat: images and metadata

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>NextJS</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
