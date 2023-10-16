e0c93a0d8443587f5c5b4925de45bc987694f107
Vladimir Orlov <86786994+happycrew@users.noreply.github.com>
2023-10-16 16:20:10 +0300

    feat: add pages navigation and redirect (#4)

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '@/styles/Home.module.scss';
 const Home = () => {
   return (
     <div className={styles.wrapper}>
-      <Heading text="Hello world" />
+      <Heading text="NextJS APP" />
     </div>
   );
 };
